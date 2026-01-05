80bbc61b576ce5b160c4d1e57e042fff257ebea7
Faheem Daruwala <faheemdaruwala1@gmail.com>
2026-01-05 15:07:39 +0530

    a

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
 
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-import { connectDB } from "./config/db.js";
 connectDB();
