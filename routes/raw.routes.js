5bfd0f4a7b437f30b26793ab6daea26e644f37f4
Zaid Ansari <zaidansari0515@gmail.com>
2026-01-06 19:07:40 +0530

    jh

--- routes/raw.routes.js
+++ routes/raw.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { createRaw, getAllRaw, updateRaw, deleteRaw } from "../controllers/raw.controller.js";
+import { createRaw, getAllRaw, deleteRaw } from "../controllers/raw.controller.js";
 import {protect} from "../middlewares/auth.middleware.js"
 
 const router = express.Router();
 
 router.post("/", protect, createRaw);
 router.get("/", protect ,getAllRaw);
-router.delete("/delete", protect, deleteRaw);
+router.delete("/delete/:id",protect, deleteRaw);
 
 export default router;
 
